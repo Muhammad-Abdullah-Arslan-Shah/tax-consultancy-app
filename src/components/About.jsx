bd242a2b2e0786798cfeb04de2663d9f828bcfdb
Muhammad-Abdullah-Arslan-Shah <154911877+Muhammad-Abdullah-Arslan-Shah@users.noreply.github.com>
2024-09-04 22:57:02 +0500

    change image name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div
     
       className="relative bg-fixed bg-cover bg-center py-20 px-6"
-      style={{ backgroundImage: "url('/src/assets/about back.jpg')", backgroundColor: 'rgba(0, 0, 0, 0.3)', backgroundBlendMode: 'overlay' }}
+      style={{ backgroundImage: "url('/src/assets/aboutBack.jpg')", backgroundColor: 'rgba(0, 0, 0, 0.3)', backgroundBlendMode: 'overlay' }}
     >
       <div className="bg-indigo-900 bg-opacity-70 p-10 rounded-lg shadow-lg max-w-4xl mx-auto text-center">
         <h2 className="text-3xl font-bold text-white mb-4">About <span className='text-amber-500'>FCC</span></h2>

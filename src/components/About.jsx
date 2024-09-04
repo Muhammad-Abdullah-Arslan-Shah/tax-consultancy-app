a6a737a3cc6c3a917eb78331a65796392366754c
Muhammad-Abdullah-Arslan-Shah <154911877+Muhammad-Abdullah-Arslan-Shah@users.noreply.github.com>
2024-09-04 23:01:20 +0500

    change about back imagr import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import back from '../assets/aboutBack.jpg';
 
 const About = () => {
   return (
     <div
-    
       className="relative bg-fixed bg-cover bg-center py-20 px-6"
-      style={{ backgroundImage: "url('/src/assets/aboutBack.jpg')", backgroundColor: 'rgba(0, 0, 0, 0.3)', backgroundBlendMode: 'overlay' }}
+      style={{
+        backgroundImage: `url(${back})`,  // Correct usage of backgroundImage
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        backgroundBlendMode: 'overlay',
+      }}
     >
       <div className="bg-indigo-900 bg-opacity-70 p-10 rounded-lg shadow-lg max-w-4xl mx-auto text-center">
-        <h2 className="text-3xl font-bold text-white mb-4">About <span className='text-amber-500'>FCC</span></h2>
+        <h2 className="text-3xl font-bold text-white mb-4">
+          About <span className="text-amber-500">FCC</span>
+        </h2>
         <p className="text-xl text-white">
           At FCC, we understand that navigating the intricate landscape of finance, taxation, and accounting can be a daunting task.
           That's where we come in—your reliable partner in financial excellence, with a commitment to precision, transparency, and growth.
